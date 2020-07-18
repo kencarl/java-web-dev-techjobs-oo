cfcacdb23cfa880a485074a9ed4816f5c01b657d
kencarl <63323859+kencarl@users.noreply.github.com>
2020-07-18 15:16:25 -0400

    2nd commit

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -4,29 +4,49 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.launchcode.techjobs_oo.*;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class JobTest {
+    Job myJob;
+    Job job2;
 
-    Job myJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-    Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
 
+    @Before
+    public void setUp() {
+        myJob = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
+    }
     @Test
     public void testJobName(){
         assertEquals("Product tester", myJob.getName());
     }
-//    @Test
+    //@Test
 //    public void testJobEmployer(){
 //        assertEquals(myJob.getEmployer(), myJob.equals("ACME"));
 //    }
     @Test
+    public void testJobConstructorSetsAllFields(){
+        assertEquals("Product tester", myJob.getName());
+        assertEquals("ACME", myJob.getEmployer().toString());
+        assertEquals("Desert", myJob.getLocation().toString());
+        assertEquals("Quality control", myJob.getPositionType().toString());
+        assertEquals("Persistence", myJob.getCoreCompetency().toString());
+
+    }
+    @Test
     public void isIdCorrect(){
-        assertEquals(1, myJob.getId());
+        assertEquals(13, myJob.getId());
+        assertEquals(14, job2.getId());
     }
     @Test
     public void isToStringGood(){
         assertEquals("Product tester", myJob.toString());
+    }
+    @Test
+    public void testJobsForEquality(){
+        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
+        Job job4 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
+        assertFalse(job3.getId() == job4.getId());
     }
     @Test
     public void isEqualsTrue(){
